fba299603c103bd0a25f19bbedf524129eac841c
Barbara Imperatori <barbarabelen.i@gmail.com>
2019-01-03 13:55:17 -0300

    función capturar movimiento de jugador completada

--- js/juego.js
+++ js/juego.js
@@ -115,15 +115,28 @@ Juego.capturarMovimiento = function(tecla) {
   // El movimiento esta determinado por la velocidad del jugador
   if (tecla == 'izq') {
     movX = -velocidad;
+    this.jugador.sprite = 'imagenes/auto_rojo_izquierda.png';
+    this.jugador.ancho = 30;
+    this.jugador.alto = 15;
+
   }
   if (tecla == 'arriba') {
     movY = -velocidad;
+    this.jugador.sprite = 'imagenes/auto_rojo_arriba.png';
+    this.jugador.ancho = 15;
+    this.jugador.alto = 30;
   }
   if (tecla == 'der') {
     movX = velocidad;
+    this.jugador.sprite = 'imagenes/auto_rojo_derecha.png';
+    this.jugador.ancho = 30;
+    this.jugador.alto = 15;
   }
   if (tecla == 'abajo') {
     movY = velocidad;
+    this.jugador.sprite = 'imagenes/auto_rojo_abajo.png';
+    this.jugador.ancho = 15;
+    this.jugador.alto = 30;
   }
 
   // Si se puede mover hacia esa posicion hay que hacer efectivo este movimiento
