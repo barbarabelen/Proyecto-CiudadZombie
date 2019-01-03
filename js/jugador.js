d82afe27eda8880717edf1b3998aa7b68c02771a
Barbara Imperatori <barbarabelen.i@gmail.com>
2019-01-03 15:57:47 -0300

    funcion perder vidas

--- js/jugador.js
+++ js/jugador.js
@@ -13,4 +13,8 @@ var Jugador = {
   // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
   // y todo lo que haga falta para que cumpla con sus responsabilidades
 
+  perderVidas: function(cantVidas){
+    this.vidas -= cantVidas;
+  }
+
 }
